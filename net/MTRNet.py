e95333c20447b466a8c7e46264b333ea8100be08
user815886724 <815886724@qq.com>
2021-09-20 17:26:07 +0800

    更新

--- net/MTRNet.py
+++ net/MTRNet.py
@@ -664,7 +664,7 @@ class TransformerVision(nn.Module):
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, active_layer=active_layer, drop=drop) if token_mlp=='ffn' else LeFF(dim,mlp_hidden_dim,active_layer=active_layer, drop=drop)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, active_layer=active_layer, drop=drop) if token_mlp=='ffn' else LeFF(dim,mlp_hidden_dim,active_layer=active_layer)
 
 
     def forward(self, x, mask=None):
